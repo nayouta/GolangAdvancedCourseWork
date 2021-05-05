285bbeb60524cf85252e730536cc9de4d2dd758f
Nayouta <2409785441@qq.com>
2021-05-05 20:58:55 +0800

    Fix existing problems, including package naming and return value

--- Week02/HandleSQLError/HandleSQLError.go
+++ Week02/HandleSQLError/HandleSQLError.go
@@ -1,37 +1,46 @@
-package HandleSQLError
+//包名称命名为小写
+package handleSQLError
 
 import (
 	"database/sql"
 	xerrors "github.com/pkg/errors"
 )
 
-
-type SQLOperationError struct{
+type SQLOperationError struct {
 	error
 }
 
-func ( *SQLOperationError) Error() string {
+func (*SQLOperationError) Error() string {
 	return "SQLOperation error occured"
 }
 
-func sqlOperation() (bool, error){
-	return false, sql.ErrNoRows
+type ValueFromTable struct {
+
 }
 
-func Test0 () error{
-	if ok, err := sqlOperation(); ok == false{
+func (*ValueFromTable) ResultDisplay(){
+
+}
+
+//对数据库操作API定义中的返回值一般为 (数据, error)的形式
+func sqlOperation() (ValueFromTable, error) {
+	return ValueFromTable{}, sql.ErrNoRows
+}
+
+func Test0() error {
+	if value, err := sqlOperation(); err != nil {
 		//使用pkg/errors提供的wrap方法进行包装
-		return xerrors.Wrapf(err,"Test0 SQLOperation failed ")	//抛给上层的同时，记录当前信息
+		return xerrors.Wrapf(err, "Test0 SQLOperation failed ") //抛给上层的同时，记录当前信息
+	}else{
+		//使用value
+		value.ResultDisplay()
 	}
 	return nil
 }
 
 //返回自定义Error的指针
-func Test1() *SQLOperationError{
+func Test1() *SQLOperationError {
 	return &SQLOperationError{
 		Test0(),
 	}
 }
-
-
-
--- Week02/main.go
+++ Week02/main.go
@@ -5,19 +5,19 @@ import (
 	"errors"
 	"fmt"
 	errors2 "github.com/pkg/errors"
-	"main.go/HandleSQLError"
+	"main.go/handleSQLError"
 )
 
-var errMy = errors.New("My");
+var errMy = errors.New("My")
 
-func main()  {
-	if err := HandleSQLError.Test0() ; err != nil{
+func main() {
+	if err := handleSQLError.Test0(); err != nil {
 		//1、直接输出err信息
 		fmt.Printf("main:%+v \n", err)
 		//2、调用Is判断
 		fmt.Printf("error.Is Result : %+v \n", errors2.Is(err, sql.ErrNoRows))
 		//3、调用AS判断
-		fmt.Printf("error.As Result : %+v \n", errors2.As(HandleSQLError.Test1(), &sql.ErrNoRows))
+		fmt.Printf("error.As Result : %+v \n", errors2.As(handleSQLError.Test1(), &sql.ErrNoRows))
 		//4、查询根因
 		fmt.Printf("error.Cause Is : %+v \n", errors2.Cause(err))
 	}
